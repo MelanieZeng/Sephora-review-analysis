66c7b35c54551aa5f0f38e61e0a503890d627a31
Michael O'Brien <39789554+michaelobr@users.noreply.github.com>
2018-06-15 22:39:58 -0400

    Update Sephora.py V1.1
    Removed printing from the program, and instead now all data is printed and saved to a file called data.txt.

--- sephora.py
+++ sephora.py
@@ -1,34 +1,48 @@
 import requests
 from bs4 import BeautifulSoup
 
-#loop all pages
+#Loop all pages
 for page_num in range(7):
-	r = requests.get('https://www.consumeraffairs.com/cosmetics/sephora.html?page=' + str(page_num))
-	
-	soup = BeautifulSoup(r.text, 'lxml')
-
-	#find reviewIDs:
-	reviewIDs = soup.find_all(class_='rvw js-rvw')
-	for id in reviewIDs:
-		print (id['data-id'])
-	
-	#find all ratings (need to filter out integer)
-	rating = soup.find_all(class_='stars-rtg stars-rtg--sm')
-	for a in rating:
-		print (a['data-rating'])
+    r = requests.get('https://www.consumeraffairs.com/cosmetics/sephora.html?page=' + str(page_num))
     
-    #find locations
-	location = soup.find_all(class_='rvw-aut__inf-nm')
-	for all in location:
-		print (all.string)
+    soup = BeautifulSoup(r.text, 'lxml')
 
-	#find comments (need to get the second paragraph)
-	comment = soup.find_all(class_='rvw-bd ca-txt-bd-2' or 'js-collapsed')
-	for n in comment:
-		print (n.p)
+    #Storage arrays
+    array_reviewID = []
+    array_rating = []
+    array_location = []
+    array_comment = []
+    array_helpful = []
 
-	#number of people found this review helpful
-	helpful = soup.find_all(class_='rvw-foot__helpful-count js-helpful-count ca-txt--clr-gray')
-	for h in helpful:
-		print (h.strong)
+    #Find reviewIDs:
+    reviewIDs = soup.find_all(class_='rvw js-rvw')
+    for id in reviewIDs:
+        array_reviewID.append(id['data-id'])
+    
+    #Find all ratings (need to filter out integer)
+    rating = soup.find_all(class_='stars-rtg stars-rtg--sm')
+    for a in rating:
+        array_rating.append(a['data-rating'])
 
+    #Find locations
+    location = soup.find_all(class_='rvw-aut__inf-nm')
+    for all in location:
+        array_location.append(all.string)
+
+    #Find comments (TODO: need to get the second paragraph)
+    comment = soup.find_all(class_='rvw-bd ca-txt-bd-2' or 'js-collapsed')
+    for n in comment:
+        array_comment.append(n.p)
+
+    #The number of people that found this review helpful
+    helpful = soup.find_all(class_='rvw-foot__helpful-count js-helpful-count ca-txt--clr-gray')
+    for h in helpful:
+        array_helpful.append(h.strong)
+
+    #Data printer
+    datafile = open ("data.txt", 'a', encoding="utf-8")   
+    for iterator in range(len(array_reviewID)):
+        datafile.write ('Rating ID: ' + array_reviewID[iterator] + '\n' + 'Rating Score: ' + array_rating[iterator] + '\n' + 'Name and Location: ' + array_location[iterator] + '\n' + 'Comment: ' + str(array_comment[iterator]) + '\n' + str(array_helpful[iterator]) + ' people found this review helpful. \n \n')
+    datafile.close()
+
+    
