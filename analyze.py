f2161c71ae89b6792dae0fe9435ba88bd6464adf
MelanieZeng <37940713+MelanieZeng@users.noreply.github.com>
2018-07-01 01:09:57 -0600

    analyze.py V1.1
    Analyze the Sephora customer reviews by using nltk.

--- analyze.py
+++ analyze.py
@@ -1,14 +1,32 @@
 import nltk
+import nltk.corpus  
+from nltk.text import Text  
 from nltk import *
 
-# Import the data we gathered using sephora.py in read-only mode
-comment_file = open("comment.txt", "r", encoding='utf-8')
-data_text = comment_file.read()
-comment_file.close()
+commentfile = open("comment.txt", 'r', encoding="utf-8")
 
-# Tokenize the dataset 
-tokens = nltk.word_tokenize(data_text)
+comment_r = commentfile.read()
 
-# Create a frequency distribution 
-fdist = FreqDist(tokens)
-print(fdist.most_common(50))	
+commentfile.close()
+
+tokens = nltk.word_tokenize(comment_r)
+
+comment_freq = FreqDist(tokens)
+print (comment_freq)
+
+comment_freq = FreqDist(tokens)
+comment_words = comment_freq.most_common(150)
+print (comment_words)
+
+set_words = set(tokens)
+long_words = [w for w in set_words if len(w)>15]
+print (long_words)
+
+comment_text = Text(tokens)
+comment_text.concordance('told')
+comment_text.concordance('refund')
+comment_text.concordance('shipping')
+comment_text.concordance('samples')
+comment_text.concordance('gift')
+comment_text.concordance('package')
+comment_text.concordance('gift-with-purchase')
